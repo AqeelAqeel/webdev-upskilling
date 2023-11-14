f2b64d085dc2374510a64799b199aee5b78e799c
Aqeel Ali <aqeelali0312@gmail.com>
2023-11-13 21:47:42 -0800

    testing clicking back

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const headline = document.querySelector('h1');
+    const originalText = headline.textContent; // Store the original text
+
     headline.addEventListener('click', function() {
-        headline.textContent = "Well butter my butt and call me a biscuit, you clicked me!";
+        // Check if the text is currently the original text
+        if (headline.textContent === originalText) {
+            // If it is, change it to the new text
+            headline.textContent = "Well butter my butt and call me a biscuit, you clicked me!";
+        } else {
+            // If it's not the original text, change it back
+            headline.textContent = originalText;
+        }
     });
 });
